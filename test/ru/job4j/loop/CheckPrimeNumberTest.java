bc83670c805f0135fecf000fbfe0fd43c92b34ac
olgaprokhorova <olya.prokhorova@gmail.com>
2020-10-12 00:29:37 +0300

    5.5. Простое число [#375996]

--- test/ru/job4j/loop/CheckPrimeNumberTest.java
+++ test/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -23,4 +23,16 @@ public class CheckPrimeNumberTest {
         boolean rsl = CheckPrimeNumber.check(1);
         assertThat(rsl, is(false));
     }
+
+    @Test
+    public void when9() {
+        boolean rsl = CheckPrimeNumber.check(9);
+        assertThat(rsl, is(false));
+    }
+
+    @Test
+    public void when2() {
+        boolean rsl = CheckPrimeNumber.check(2);
+        assertThat(rsl, is(true));
+    }
 }
